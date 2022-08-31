ec7e98614ba9d215e4d569157c475a9b1df5ccbe
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2022-08-31 09:04:22 -0300

    Implementação de auditoria para datas de criação e de última atualização

--- src/main/kotlin/com/example/auditoria/entity/ProdutoEntity.kt
+++ src/main/kotlin/com/example/auditoria/entity/ProdutoEntity.kt
@@ -1,9 +1,14 @@
 package com.example.auditoria.entity
 
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
 @Table(name = "produto")
+@EntityListeners(AuditingEntityListener::class)
 class ProdutoEntity(
 
     @Id
@@ -15,6 +20,14 @@ class ProdutoEntity(
     var nome: String? = null,
 
     @Column(name = "descricao")
-    var descricao: String? = null
+    var descricao: String? = null,
+
+    @Column(name = "criado_em", nullable = false, updatable = false)
+    @CreatedDate
+    var criadoEm: LocalDateTime? = null,
+
+    @Column(name = "atualizado_em", nullable = false)
+    @LastModifiedDate
+    var atualizadoEm: LocalDateTime? = null
 
 )
--- src/main/resources/db/migration/V1.0_0__create_schema.sql
+++ src/main/resources/db/migration/V1.0_0__create_schema.sql
@@ -2,5 +2,7 @@ create table produto
 (
     id serial not null primary key,
     nome varchar(60) not null,
-    descricao varchar(60) not null
+    descricao varchar(60) not null,
+    criado_em timestamp not null,
+    atualizado_em timestamp not null
 );
